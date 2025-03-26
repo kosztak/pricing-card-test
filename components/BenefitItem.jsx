70b16e9c010c2effb42962e5b9cc034d7d8a35bc
Ákos <kokoszta77@gmail.com>
2025-03-26 12:23:19 +0100

    Icons added to benefits

--- components/BenefitItem.jsx
+++ components/BenefitItem.jsx
@@ -1,7 +1,14 @@
+import CheckIcon from "./icons/CheckIcon";
+import CrossIcon from "./icons/CrossIcon";
+
+
 export default function BenefitItem({ text, hasBenefit, hasBackground }) {
     return(
         <div className={`px-[16px] py-[12px] rounded-[4px] flex gap-[8px] ${hasBackground ? 'bg-[#F6F6F6]' : ''}`}>
-            <p>{hasBenefit ? '+' : '-'}</p>
+            {hasBenefit ? 
+                <CheckIcon width={24} height={24} /> : 
+                <CrossIcon width={24} height={24} />
+            }
             <p className="font-text">{text}</p>
         </div>
     )
---
+++ components/icons/CheckIcon.jsx
@@ -0,0 +1,7 @@
+export default function CheckIcon({ width, height }) {
+    return(
+    <svg width={width || "200px"} height={height || "200px"} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4.89163 13.2687L9.16582 17.5427L18.7085 8" stroke="#2D2E2E" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    )
+}
